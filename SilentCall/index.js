10625cb543bae78607257545abfa70670ec6902c
Gilbaneco ` <josephfriskelii@gmail.com>
2025-06-28 13:04:01 -0700

    Add SilentCall Azure Function (PowerShell)

---
+++ SilentCall/index.js
@@ -0,0 +1,18 @@
+﻿module.exports = async function (context, req) {
+    const userEmail = req.query.userEmail;
+
+    if (!userEmail) {
+        context.res = {
+            status: 400,
+            body: "Missing \'userEmail\' query parameter."
+        };
+        return;
+    }
+
+    context.log(`Silent call initiated for ${userEmail}`);
+
+    context.res = {
+        status: 200,
+        body: `Silent call registered for ${userEmail}.`
+    };
+};
